9815a3df64d9201de9b1b8df118af30eb2b02704
Adm Gaygusuz <adem@ardweb.co.uk>
2017-07-21 11:24:59 +0100

    Fix data tables and doc strings

--- formatter.js
+++ formatter.js
@@ -1,5 +1,7 @@
 const Jetty = require("jetty");;
 const { PrettyFormatter } = require('cucumber');
+const DataTable = require('./node_modules/cucumber/lib/models/step_arguments/data_table').default;
+const DocString = require('./node_modules/cucumber/lib/models/step_arguments/doc_string').default;
 
 class ShowAndTellFormatter extends PrettyFormatter {
   constructor(options) {
@@ -43,16 +45,17 @@ class ShowAndTellFormatter extends PrettyFormatter {
     const colorFn = this.colorFns[status || 'location'];
     const symbol = PrettyFormatter.CHARACTERS[status || 'skipped'];
     const identifier = colorFn(symbol + ' ' + step.keyword + (step.name || ''));
-    this.logIndented(identifier + '\n', 1)
 
-    step.arguments.forEach((arg) => {
+    this.logIndented(identifier + '\n', 1);
+
+    step.arguments.forEach(arg => {
       let str;
       if (arg instanceof DataTable) {
-        str = this.formatDataTable(arg)
+        str = this.formatDataTable(arg);
       } else if (arg instanceof DocString) {
-        str = this.formatDocString(arg)
+        str = this.formatDocString(arg);
       } else {
-        throw new Error('Unknown argument type: ' + arg)
+        throw new Error('Unknown argument type: ' + arg);
       }
       this.logIndented(colorFn(str) + '\n', 3)
     })
